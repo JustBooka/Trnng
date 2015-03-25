06903f2374f745d980fca9dd678924d585ebe0ea
alexey.bukin <alexeybookin@gmail.com>
2015-03-25 16:42:44 +0300

    Image Send

--- app/src/main/java/client/github/trnng/activity/IntentTrnngActivity.java
+++ app/src/main/java/client/github/trnng/activity/IntentTrnngActivity.java
@@ -5,19 +5,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import client.github.trnng.R;
 
 /**
  * Created by alexey.bukin on 25.03.2015.
  */
-public class IntentTrnngActivity extends Activity{
-
-//    Button btnMap = (Button) findViewById(R.id.btn_map_launch);
-//    Button btnGooglePlay = (Button) findViewById(R.id.btn_store_launch);
-//    Button btnEmailClient = (Button) findViewById(R.id.btn_email_launch);
-
+public class IntentTrnngActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +19,42 @@ public class IntentTrnngActivity extends Activity{
         setContentView(R.layout.ativity_intent);
     }
 
-    public void intentLaunch(View v)
-    {
+    public void intentLaunch(View v) {
         Intent intent = null, chooser = null;
 
-        if (v.getId()==R.id.btn_map_launch)
-        {
+        if (v.getId() == R.id.btn_map_launch) {
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("geo: 59.570 , 30.190"));
-            chooser=Intent.createChooser(intent,"Launch Maps");
+            chooser = Intent.createChooser(intent, "Launch Maps");
             startActivity(chooser);
         }
 
-        if (v.getId()==R.id.btn_store_launch)
-        {
+        if (v.getId() == R.id.btn_store_launch) {
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("market://details?id=tutorial.DrawSample"));
-            chooser=Intent.createChooser(intent,"Launch Market");
+            chooser = Intent.createChooser(intent, "Launch Market");
             startActivity(chooser);
         }
 
-        if (v.getId()==R.id.btn_email_launch)
-        {
+        if (v.getId() == R.id.btn_email_launch) {
             intent = new Intent(Intent.ACTION_SEND);
             intent.setData(Uri.parse("mailto:"));
-            String [] to = {"alexeybookin@gmail.com","mfkr@mail.ru"};
+            String[] to = {"alexeybookin@gmail.com", "mfkr@mail.ru"};
             intent.putExtra(Intent.EXTRA_EMAIL, to);
             intent.putExtra(Intent.EXTRA_SUBJECT, "Test email from trnng app");
             intent.putExtra(Intent.EXTRA_TEXT, "this is test email message");
             intent.setType("message/rfc822");
-            chooser=Intent.createChooser(intent,"Launch Email Client");
+            chooser = Intent.createChooser(intent, "Launch Email Client");
+            startActivity(chooser);
+        }
+
+        if (v.getId() == R.id.sendImage) {
+            Uri imageUri = Uri.parse("android.resource://client.github.trnng/drawable/" + R.drawable.wallpaper);
+            intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+            intent.putExtra(Intent.EXTRA_TEXT,"image attached");
+            chooser=Intent.createChooser(intent,"Send Image");
             startActivity(chooser);
         }
     }
